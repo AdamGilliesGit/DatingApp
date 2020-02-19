97bcd55127d5ff51c5ed62b1db5d8ba675f97a41
Adam Gillies <adam.d.gillies@gmail.com>
2020-02-19 18:36:55 +0000

    “message”

---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using datingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
