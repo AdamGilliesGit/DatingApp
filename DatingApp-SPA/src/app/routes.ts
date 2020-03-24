5fd2412bd31a07532c1ee86a420c0037f4f9e0e1
Adam Gillies <adam.d.gillies@gmail.com>
2020-03-24 16:55:17 +0000

    End of Section 8

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -17,7 +17,6 @@ export const appRoutes: Routes = [
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
-    }
- 
-    { path: '**', redirectTo: '', pathMatch: 'full'},
+    },
+     { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
